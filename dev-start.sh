ca1f1548f82401c686cbffd045a9d5f8c1d73f22
redeemed2011 <bo@samware.com>
2018-12-12 21:32:13 -0600

    Replace "watcher" util with nodemon since the former does not function as it once did.

--- dev-start.sh
+++ dev-start.sh
@@ -7,4 +7,10 @@ set -x
 . scripts/dev-envs.sh
 
 # Run the project every time a file changes.
-watcher -depth 6 scripts/./dev-start-helper.sh
+# watcher -depth 6 scripts/./dev-start-helper.sh
+nodemon \
+  --watch . \
+  --ext '.go' \
+  --ignore 'metro' \
+  --verbose \
+  --exec ${SHELL} scripts/./dev-start-helper.sh
